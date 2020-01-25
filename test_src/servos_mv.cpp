36880581a158262510622dd2026907560b9f9b0b
xingr-orst <xingr@oregonstate.edu>
2020-01-25 10:24:13 -0800

    starting some code and updated TODOs

---
+++ test_src/servos_mv.cpp
@@ -0,0 +1,8 @@
+/*
+ * Testing program for all the servos to check if they are all properly
+ * working and in sync with each other.
+ * Also used for power testing to make sure everything is recieving and
+ * pulling safe amounts of current.
+ */
+
+#include <iostream>
