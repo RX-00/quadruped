1bbbbb0877f3a3364249702a42588b03bc856e41
RX-00 <00royxing@gmail.com>
2020-02-18 13:19:06 -0500

    wow this serial interface is not wanting to work

--- test_src/test.py
+++ test_src/test.py
@@ -2,15 +2,15 @@
 # -*- coding: utf-8 -*-
 
 '''# just some test code I found, testing it to see what's up with the serial port'''
-import Maestro.maestro as maestro
+import maestro
 import time
 
 #define servo
 
-servo = maestro.Controller(ttyStr='/dev/ttyS0')
-min_imp = 2000
-max_imp = 10000
-step_imp = 100
+#servo = maestro.Controller(ttyStr='/dev/ttyACM0')
+#min_imp = 2000
+#max_imp = 10000
+#step_imp = 100
 '''
 #infinite loop
 while 1:
@@ -33,9 +33,27 @@ servo.close
 '''
 
 # my own crap I'm testing out:
-for i in range(0, 19):
-    print("servo: {}     servo pos: {} \n".format(i, 2000))
-    servo.setTarget(0, 2000)
-    time.sleep(0.1)
+#for i in range(0, 19):
+#    print("servo: {}     servo pos: {} \n".format(i, 1600))
+#    servo.setTarget(i, 2000)
+#    time.sleep(1)
 
-servo.close
+#servo.close
+
+
+
+# WHY WON'T THIS WORK?!
+
+PORT = 6
+
+servo = maestro.Controller()
+servo.setAccel(PORT, 0)
+servo.setSpeed(PORT, 60)
+
+for i in range(1000, 3000):
+    servo.setTarget(PORT, i)
+    time.sleep(0.02)
+    print("servo: {}    servo target: {} \n".format(PORT, i))
+    print("servo: {}    servo pos: {} \n".format(PORT, servo.getPosition(6)))
+
+servo.close()
